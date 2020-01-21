5a9a0557ad71458c482fd54e022d79ee9e12857c
aidencuneo <aidencuneo@gmail.com>
2020-01-21 15:47:08 +1100

    1.4.0 Re-export and Install Script Update
    Install Script has been updated so that it now moves startup.mdcl to the correct location.
    
    MDCL 1.4.0 has been re-exported.

--- install_script.py
+++ install_script.py
@@ -34,20 +34,32 @@ elif os.path.isfile('dist/mdcl-' + str(__version__)):
 elif os.path.isfile('dist/__init__'):
     error('THERE IS A FILE IN THE WAY OF PATH '
         'dist/__init__ WHICH MUST BE REMOVED.')
+
 os.system('pyinstaller src/__init__.py')
-success('INSTALL')
+success('SOURCE HAS BEEN BUILT')
+
 os.remove('dist/__init__/__init__.exe.manifest')
 success('REMOVED MANIFEST')
+
 shutil.rmtree('build')
 success('REMOVED TEMP BUILD FOLDER')
+
 os.remove('__init__.spec')
 success('REMOVED TEMP SPEC FILE')
+
 os.rename('dist/__init__/__init__.exe', 'dist/__init__/mdcl.exe')
 success('RENAMED MAIN EXECUTABLE')
+
+if not os.path.isfile('src/startup.mdcl'):
+    error('src/startup.mdcl STARTUP FILE NOT FOUND.')
+shutil.copy2('src/startup.mdcl', 'dist/__init__/startup.mdcl')
+success('COPIED STARTUP FILE (startup.mdcl)')
+
 if not os.path.isdir('src/builtins'):
     error('src/builtins PATH NOT FOUND.')
 shutil.copytree('src/builtins', 'dist/__init__/builtins')
 success('COPIED BUILTINS')
+
 if not os.path.isdir('src/__pycache__'):
     error('src/__pycache__ PATH NOT FOUND.')
 for a in os.listdir('src/__pycache__'):
@@ -56,6 +68,7 @@ for a in os.listdir('src/__pycache__'):
     shutil.copy2('src/__pycache__/' + a, 'dist/__init__/' + a)
     os.rename('dist/__init__/' + a, remove_pycache_info('dist/__init__/' + a))
 success('COPIED AND RENAMED MAIN SOURCE FILES')
+
 try:
     os.rename('dist/__init__', 'dist/mdcl-' + str(__version__))
     success('EXPORT FOLDER RENAMED')
